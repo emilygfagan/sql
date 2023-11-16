0c0bb0c5df47d8aaea8ea9be3fb149af804de7b6
Emily Fagan <emilygfagan@gmail.com>
2023-11-15 21:32:35 -0600

    Add files via upload

---
+++ Import_Export.sql
@@ -0,0 +1,18 @@
+-- Import and Export
+/* Practicing importing a csv file
+directly into a table. First I have
+to create a table that matches the
+format of the csv file. */
+
+CREATE TABLE simple(
+a INTEGER,
+b INTEGER,
+c INTEGER
+);
+
+/* Uploaded simple csv file, running query
+to view the complete table. */
+
+SELECT * FROM simple;
+
+-- Query successful
