0975c250c63258a91cfdf0a4b8acd0528df738b4
Emily Fagan <emilygfagan@gmail.com>
2024-02-11 18:05:17 -0600

    Update Practice_3.sql

--- Practice_3.sql
+++ Practice_3.sql
@@ -1,7 +1,12 @@
-USE hr;
-  -- telling MySQL to use the hr database
+/* HR Practice SQL Code
+Assignment for Data Analytics Bootcamp */
 
--- Task 1 - List IDs, first_name, and last_name from employees working in the IT department. --
+
+USE hr; -- telling MySQL to use the hr database
+
+
+-- Task 1 - List IDs, first_name, and last_name from employees working in the IT department. 
+
 SELECT employees.employee_id AS 'Employee ID', employees.first_name AS 'First Name', employees.last_name AS 'Last Name'
   -- selecting appropriate columns and naming for aesthetics
 FROM employees LEFT JOIN departments ON employees.department_id = departments.department_id
@@ -9,7 +14,9 @@ FROM employees LEFT JOIN departments ON employees.department_id = departments.de
 WHERE departments.department_name = "IT";
   -- conditional statement where department is IT
 
--- Task 2: Extract the minimum and maximum salaries of each department. --
+
+-- Task 2: Extract the minimum and maximum salaries of each department. 
+
 SELECT departments.department_name AS 'Department Name', MIN(employees.salary) AS 'Minimum Salary', MAX(employees.salary) AS 'Maximum Salary'
   -- selecting appropriate columns and naming for aesthetics
 FROM employees LEFT JOIN departments ON employees.department_id = departments.department_id
@@ -19,7 +26,9 @@ GROUP BY departments.department_name
 ORDER BY MAX(employees.salary);
   -- ordering by the maximum salary
 
--- Task 3: Identify the top 10 cities that have the highest number of employees. --
+
+-- Task 3: Identify the top 10 cities that have the highest number of employees. 
+
 SELECT COUNT(employees.employee_id) AS 'Number Employees', locations.city AS 'City', departments.department_name AS 'Department Name'
   -- selecting appropriate columns and naming for aesthetics
 FROM employees INNER JOIN departments ON employees.department_id = departments.department_id
@@ -33,7 +42,9 @@ ORDER BY COUNT(employees.employee_id) DESC
 LIMIT 10;
   -- only displaying the top 10
 
--- Task 4: List employee_id, first_name, last_name of employees whose last day working was 1999-12-31. --
+
+-- Task 4: List employee_id, first_name, last_name of employees whose last day working was 1999-12-31. 
+
 SELECT employees.employee_id AS 'Employee ID', employees.first_name AS 'First Name', employees.last_name AS 'Last Name'
   -- selecting appropriate columns and naming them for aesthetics
 FROM employees INNER JOIN job_history ON employees.employee_id = job_history.employee_id
@@ -41,8 +52,12 @@ FROM employees INNER JOIN job_history ON employees.employee_id = job_history.emp
 WHERE end_date = '1999-12-31';
   -- conditional statement where last day was December 31st, 1999
 
--- Task 5: Extract the employee IDs, first names, department names, and total experience of all those employees who have completed at least 25 years in the organization. --
-SELECT employees.employee_id AS 'Employee ID', employees.first_name AS 'First Name', departments.department_name AS 'Department', TIMESTAMPDIFF(year, employees.hire_date, '2023-07-26') AS 'Number of Years'
+
+/* Task 5: Extract the employee IDs, first names, department names, and total experience of all those employees 
+who have completed at least 25 years in the organization. */
+
+SELECT employees.employee_id AS 'Employee ID', employees.first_name AS 'First Name', departments.department_name AS 'Department', 
+  TIMESTAMPDIFF(year, employees.hire_date, '2023-07-26') AS 'Number of Years'
   -- selecting appropriate columns and naming them for aesthetics
 FROM employees INNER JOIN departments ON employees.department_id = departments.department_id
   -- joining employees and departments tables
